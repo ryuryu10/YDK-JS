dbe6737b96343a4e6a7e68e1aaa60c8625854da4
RYUSS - ABCD <ryuryu10@naver.com>
2022-05-28 09:31:55 +0900

    5.5 commit

---
+++ 5.5(249)2.js
@@ -0,0 +1,21 @@
+function CoolModule() {
+    var something = "cool";
+    var another = [1,2,3];
+
+    function doSomething() {
+        console.log( something );
+    }
+    function doAnother() {
+        console.log( another.join( " ! ") );
+    }
+
+    return {
+        doSomething : doSomething,
+        doAnother : doAnother
+    };
+}
+
+var foo = CoolModule();
+
+foo.doSomething();
+foo.doAnother();
